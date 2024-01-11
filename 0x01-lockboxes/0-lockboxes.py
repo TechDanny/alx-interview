7653ace2ffdee1c9df596c88271fd1bb4f9023de
root <kokidanny15@gmail.com>
2024-01-11 16:49:12 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """
+    This function unlocks all the boxes
+    """
+    boxIsOpened = set([0])
+    check_keys = boxes[0]
+
+    while check_keys:
+        current_key = check_keys.pop()
+        if 0 <= current_key < len(boxes) and current_key not in boxIsOpened:
+            boxIsOpened.add(current_key)
+            check_keys.extend(boxes[current_key])
+    return len(boxIsOpened) == len(boxes)
