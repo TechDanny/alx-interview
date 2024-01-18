4a9bdc479139919992b1e620b682b6c6d0afc89d
root <kokidanny15@gmail.com>
2024-01-18 13:08:14 +0300

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Minimum operation
+"""
+
+
+def minOperations(n):
+    """
+    calculates the fewest number of operations
+    """
+    if n <= 0:
+        return 0
+    value = n
+    for x in range(2, int(n ** 0.5) + 1):
+        if n % x == 0:
+            value = min(value, minOperations(x) + n // x)
+    return value
