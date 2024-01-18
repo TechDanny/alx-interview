97505ba146cc94f6cb9a33da7b854c548950449a
root <kokidanny15@gmail.com>
2024-01-18 13:09:25 +0300

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,7 +8,7 @@ def minOperations(n):
     """
     calculates the fewest number of operations
     """
-    if n <= 0:
+    if n <= 1:
         return 0
     value = n
     for x in range(2, int(n ** 0.5) + 1):
