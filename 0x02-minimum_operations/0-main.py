773bfffd9c57f3c91b4d432e6bc89eab87e53480
root <kokidanny15@gmail.com>
2024-01-18 22:33:07 +0300

    minimum operation

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -6,7 +6,7 @@ Main file for testing
 minOperations = __import__('0-minoperations').minOperations
 
 n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
