141bddb13e2113b85ef7e5b67ccfbf189c0c7090
root <kokidanny15@gmail.com>
2024-02-18 00:26:39 +0300

    starwars characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,53 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let persons = [];
+const name = [];
+
+const requestChar = async () => {
+  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const Body = JSON.parse(body);
+      persons = Body.characters;
+      resolve();
+    }
+  }));
+};
+
+const requestPersons = async () => {
+  if (persons.length > 0) {
+    for (const x of persons) {
+      await new Promise(resolve => request(x, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const Body = JSON.parse(body);
+          name.push(Body.name);
+          resolve();
+        }
+      }));
+    }
+  } else {
+    console.error('Error: Characters missing');
+  }
+};
+
+const getNames = async () => {
+  await requestChar();
+  await requestPersons();
+
+  for (const x of name) {
+    if (x === name[name.length - 1]) {
+      process.stdout.write(x + '\n');
+    } else {
+      process.stdout.write(x + '\n');
+    }
+  }
+};
+
+getNames();
