8b6ca3168ea3df3e663504e8760380734aecdd7d
root <kokidanny15@gmail.com>
2024-02-28 13:34:29 +0300

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Change comes from within
+"""
+
+
+def makeChange(coins, total):
+    """
+    determines the fewest number of coins needed to meet a given amount
+    """
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+    for x in range(1, total + 1):
+        for n in coins:
+            if x - n >= 0:
+                dp[x] = min(dp[x], 1 + dp[x - n])
+    return dp[total] if dp[total] != total + 1 else -1
