d0d41e22b82b2ca6208c312581663a768299370a
root <kokidanny15@gmail.com>
2024-02-28 15:39:45 +0300

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,10 +8,10 @@ def makeChange(coins, total):
     """
     determines the fewest number of coins needed to meet a given amount
     """
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0
     if total <= 0:
         return 0
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
 
     for x in range(1, total + 1):
         for n in coins:
