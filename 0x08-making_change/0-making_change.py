daacd4b07fec114a5a4f13881164a2662abb4f1b
root <kokidanny15@gmail.com>
2024-02-28 13:51:15 +0300

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,6 +10,9 @@ def makeChange(coins, total):
     """
     dp = [total + 1] * (total + 1)
     dp[0] = 0
+    if total <= 0:
+        return 0
+
     for x in range(1, total + 1):
         for n in coins:
             if x - n >= 0:
